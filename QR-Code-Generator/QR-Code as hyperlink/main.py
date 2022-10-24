7c061ce834cf9b5f8fc3b5ba703266ed94cf024b
Wasim Zaman <wasimxaman13@gmail.com>
2022-10-24 23:25:44 +0500

    new project added with first commit

--- QR-Code-Generator/QR-Code as hyperlink/main.py
+++ QR-Code-Generator/QR-Code as hyperlink/main.py
@@ -1,19 +1,15 @@
-# import qrcode
-#
-# code = qrcode.QRCode(version=1, box_size=36, border=6)
-#
-# data = 'https://www.google.com'
-# code.add_data(data)
-#
-# code.make()
-#
-# image = code.make_image(fill_color = 'black', back_color='white')
-#
-# image.save('./image.png')
+import qrcode
 
-# Import module
-import wifi_qrcode_generator as qr
+code = qrcode.QRCode(version=1, box_size=36, border=6)
 
-# Use wifi_qrcode() to create a QR image
-code = qr.wifi_qrcode('Muhammad 1', False, 'WPA', 'malakand')
-code.save("./wifiimage.png")
+data = 'https://www.google.com'
+code.add_data(data)
+
+code.make()
+
+image = code.make_image(fill_color = 'black', back_color='white')
+
+image.save('./image.png')
+
+
+
---
+++ QR-Code-Generator/QR-Code for wifi/main.py
@@ -0,0 +1,2 @@
+import wifi_qrcode_generator as qr
+image = qr.wifi_qrcode('Muhammad 1', False, 'WPA', 'malakand').save('./wifi_image.png')
